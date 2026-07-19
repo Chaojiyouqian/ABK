9d7754d4eeefdb71d450556bdd35eba31bf391cc
fanziyun <yan13517849798yan@163.com>
2026-07-20 02:15:21 +0800

    Update build.gradle.kts

--- app_dev/build.gradle.kts
+++ app_dev/build.gradle.kts
@@ -11,7 +11,7 @@ val githubClientId = providers.gradleProperty("ABK_GITHUB_CLIENT_ID")
     .orElse(providers.environmentVariable("ABK_GITHUB_CLIENT_ID"))
     .orElse("Ov23li8skGo6AFPBeSTh")
 val appVersionCode = 10025
-val appVersionName = "1.2.5-dev"
+val appVersionName = "1.2.5"
 val appUpdateMetadataUrl = providers.environmentVariable("ABK_APP_UPDATE_METADATA_URL")
     .orElse("https://raw.githubusercontent.com/xingguangcuican6666/ABK/dev/version.json")
 val appBuildTimestamp = providers.environmentVariable("ABK_APP_BUILD_TIMESTAMP")
